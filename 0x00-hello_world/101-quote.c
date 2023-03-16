a73d2176e87768c493d55a50e4d46dcdb1708b04
Sana Assim <sanaassim1@gmail.com>
2023-03-16 02:11:13 -0700

    Fix: a C program that prints a string to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fwrite(str, 1, sizeof(str), stderr);
 	return (1);
