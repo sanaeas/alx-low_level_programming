0803cb2070c9665b74fe9356715ca86ad46ffc88
Sana Assim <sanaassim1@gmail.com>
2023-03-16 02:13:59 -0700

    Change variable name

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char s[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, 1, sizeof(str), stderr);
+	fwrite(s, 1, sizeof(s), stderr);
 	return (1);
 }
