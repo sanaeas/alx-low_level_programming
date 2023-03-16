853a1bc7b8b5a367950d0337b97bba689636ea51
Sana Assim <sanaassim1@gmail.com>
2023-03-16 02:06:57 -0700

    Print a string to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, 1, sizeof(str), stderr);
+	return (1);
+}
