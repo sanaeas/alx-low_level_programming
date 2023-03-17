3bfd4e47f2117da6fb757e1fd854300e1004ad79
Sana Assim <sanaassim1@gmail.com>
2023-03-17 04:54:13 -0700

    Print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return 0
+ */
+int main(void)
+{
+	int n, m, n1, m1;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 48; m < 58; m++)
+		{
+			for (n1 = 48; n1 < 58; n1++)
+			{
+				for (m1 = 48; m1 < 58; m1++)
+				{
+					if ((n == n1 && m == m1) ||
+					(n > n1 || m > m1))
+						continue;
+					putchar(n);
+					putchar(m);
+					putchar(' ');
+					putchar(n1);
+					putchar(m1);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
