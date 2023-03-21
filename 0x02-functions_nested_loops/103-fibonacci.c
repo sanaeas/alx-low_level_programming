f5607ab5383d61c016864a158cffd8dbfb4a9924
Sana Assim <sanaassim1@gmail.com>
2023-03-21 08:16:17 -0700

    Print the sum of the even-valued of Fibonacci numbers bellow 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the sum of the even-valued of Fibonacci numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long num1, num2, sum, evenSum;
+	int i;
+
+	num1 = 0;
+	num2 = 1;
+	sum = 0;
+	evenSum = 0;
+
+	while (c <= 4000000)
+	{
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+		if (sum % 2 == 0)
+			evenSum += sum;
+	}
+	printf("%lu\n", evenSum);
+}
