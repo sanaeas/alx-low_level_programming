6a15003d71465afd88ab4b07e8ca7214bd83893e
Sana Assim <sanaassim1@gmail.com>
2023-03-21 01:20:17 -0700

    Print 10 times the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - a function, prints 10 times the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j = 123; j++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
