27bc6a36f59e55fb055ee2c3a9b81c4061b5ec8a
Sana Assim <sanaassim1@gmail.com>
2023-03-23 08:15:01 -0700

    Print a given number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,17 +9,16 @@
  */
 void print_number(int n)
 {
+	unsigned int num = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= (-1);
+		num = -num;
 	}
 
-	if (n > 999)
-		_putchar((n / 1000) + '0');
-	if (n > 99)
-		_putchar(((n / 100) % 10) + '0');
-	if (n > 9)
-		_putchar(((n / 10) % 10) + '0');
-	_putchar((n % 10) + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + 48);
 }
