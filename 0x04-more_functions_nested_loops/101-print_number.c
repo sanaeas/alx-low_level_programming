be385d426cb2480351b6fe70bccea63cdcae4caf
Sana Assim <sanaassim1@gmail.com>
2023-03-23 07:44:33 -0700

    Print a given number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - Print the given number
+ *
+ * @n - a given number
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= (-1);
+	}
+
+	if (num > 999)
+		_putchar(((n / 1000) % 10) + '0');
+	if (num > 99)
+		_putchar(((n / 100) % 10) + '0');
+	if (num > 9)
+		_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
+	_putchar('\n');
+}
