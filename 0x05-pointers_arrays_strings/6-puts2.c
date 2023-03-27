cb9b2515db3c3a5252440fb3fcf51419fdd0578a
Sana Assim <sanaassim1@gmail.com>
2023-03-27 04:30:00 -0700

    Print every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other character of a string
+ *
+ * @str: a pointer to a string
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '0')
+	{
+		_putchar(str[i]);
+		if (str[i + 1] == '\0')
+			break;
+		i += 2;
+	}
+}
