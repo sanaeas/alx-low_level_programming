0123dd0102332ab7b1226212f7e755e5168f6fb9
Sana Assim <sanaassim1@gmail.com>
2023-03-27 03:23:45 -0700

    Print the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: a pointer to a string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (len >= 0)
+	{
+		if (s[len] == '\0')
+		       break;
+		else
+			len++;	
+	}
+	return (len);
+}
