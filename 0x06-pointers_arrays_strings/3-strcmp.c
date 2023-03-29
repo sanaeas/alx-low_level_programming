1e9d90346fc3218742d0c7e9f05a08c2615cafb3
Sana Assim <sanaassim1@gmail.com>
2023-03-29 03:44:37 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: the difference
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+
+	if (s1[i] != '\0')
+		return (s1[i]);
+	else if (s2[i] != '\0')
+		return (s2[i]);
+
+	return (0);
+}
