94adeada47bc19639eb51f263aa9d617b4d5d1b9
Sana Assim <sanaassim1@gmail.com>
2023-03-29 03:09:36 -0700

    copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to copy
+ *
+ * return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}
