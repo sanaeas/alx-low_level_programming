588909d8072aa1b9cbaa1d2787fe809f5b588fef
Sana Assim <sanaassim1@gmail.com>
2023-03-30 02:49:30 -0700

    Fix: Capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,30 +3,30 @@
 /**
  * cap_string - a function that capitalizes all words of a string
  *
- * @str: a given string
+ * @s: a given string
  *
  * Return: Capitalized string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
 	char sep[] = {' ', 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		j = 0;
 		while (sep[j] != '\0')
 		{
-			if (str[i] == sep[j])
+			if (s[i] == sep[j])
 			{
-				if (str[i + 1] >= 97 && str[i + 1] <= 122)
-					str[i + 1] -= 32;
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] -= 32;
 				break;
 			}
 			j++;
 		}
 		i++;
 	}
-	return (str);
+	return (s);
 }
