fb8e4422cf0771f0f414328723e6651e79fc1222
Sana Assim <sanaassim1@gmail.com>
2023-04-08 02:38:04 -0700

    A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
