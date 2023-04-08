ba44624755468063405cb690a81632114c53c9b1
Sana Assim <sanaassim1@gmail.com>
2023-04-08 03:58:38 -0700

    Add positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,23 +13,16 @@ int main(int argc, char *argv[])
 {
 	int i, result;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-
 	i = 1;
 	result = 0;
 	while (i < argc)
 	{
-		if (!atoi(argv[i]))
+		if (!(atoi(argv[i])))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		if (atoi(argv[i]) > 0)
-			result += atoi(argv[i]);
+		result += atoi(argv[i]);
 		i++;
 	}
 	printf("%d\n", result);
