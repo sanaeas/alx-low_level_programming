4a2eb1bc55ebb266c3b7aae9cb8432f064a66667
Sana Assim <sanaassim1@gmail.com>
2023-04-19 04:32:41 -0700

    Execute a function on an array elements

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 
-#endif /* MAIN */
+#endif /* FUNCTION_POINTERS_H */
