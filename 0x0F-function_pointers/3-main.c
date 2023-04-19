66534d19d6f6979368eacbc29d588582a9fad7bf
Sana Assim <sanaassim1@gmail.com>
2023-04-19 07:46:02 -0700

    A program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,6 +20,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
 	{
