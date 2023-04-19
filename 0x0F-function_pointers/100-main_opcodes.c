cd564753a5d495afbea067dc9836fac4dafeef37
Sana Assim <sanaassim1@gmail.com>
2023-04-19 08:51:51 -0700

    a program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,8 +11,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	unsigned char *ptr;
+	int num_bytes, i;
+	char *ptr;
 
 	if (argc != 2)
 	{
@@ -20,19 +20,19 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	bytes = atoi(argv[1]);
-	if (bytes < 0)
+	num_bytes = atoi(argv[1]);
+	if (num_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	ptr = (unsigned char *)main;
-	for (i = 0; i < bytes; i++)
+	ptr = (char *)main;
+	for (i = 0; i < num_bytes; i++)
 	{
-		if (i == (bytes - 1))
+		if (i == (num_bytes - 1))
 		{
-			printf("%02x ", *ptr++);
+			printf("%02x\n", *ptr++);
 			break;
 		}
 		printf("%02x ", *ptr++);
