2c8751b6f6753f1834c015d2ba7c8b570fc4fcbf
Sana Assim <sanaassim1@gmail.com>
2023-04-19 04:40:51 -0700

    Execute a function on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (!array || !action)
 		return;
 
-	i = 0;
-	while (i < size)
-		action(array[i++];
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
