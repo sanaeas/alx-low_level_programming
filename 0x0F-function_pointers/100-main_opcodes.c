dc5c8dfda47eb83f49ad3eec376abd19d8c0e86e
Sana Assim <sanaassim1@gmail.com>
2023-04-19 08:36:54 -0700

    A program that prints its own opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints its own opcode
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, i;
+	unsigned char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (unsigned char *)main;
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", *ptr++);
+	printf("\n");
+	return (0);
+}
