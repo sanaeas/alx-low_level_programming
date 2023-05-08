6d265b8ac78ed2fe410eb9e27c27e9659ddfe54b
Sana Assim <sanaassim1@gmail.com>
2023-05-08 06:25:10 -0700

    Header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <sys/types.h>
+#include<sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
