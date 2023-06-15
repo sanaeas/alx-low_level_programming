44b3951af3973766aac36ca4c042758f0637160a
Sana Assim <sanaassim1@gmail.com>
2023-06-15 03:40:42 -0700

    Calculate the sum of all the data of a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data of a doubly linked list
+ *
+ * @head: the head node
+ *
+ * Return: Sum of all data | 0 (list is empty)
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
