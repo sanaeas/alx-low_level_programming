32700a7c6ef2efb7262fd29ea64e0b9fc2c090de
Sana Assim <sanaassim1@gmail.com>
2023-06-15 04:47:04 -0700

    Fix: head node is NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+	if (!h || !(*h))
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
