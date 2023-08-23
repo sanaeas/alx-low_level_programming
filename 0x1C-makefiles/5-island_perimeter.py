22ee982475903a4135ac5ac785464a80da47b0cd
Sana Assim <sanaassim1@gmail.com>
2023-08-23 03:22:22 -0700

    Calculate the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module that contains island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
